44e51127f14a60bf9f32f560ca8bf8e711d2172a
frosch <frosch@openttd.org>
2018-04-08 16:43:02 +0200

    Add: Optional environment variable HOOKS_DIR to use instead of GIT_DIR/hooks

--- hooks/check-commits.sh
+++ hooks/check-commits.sh
@@ -7,13 +7,14 @@
 set -e
 
 # --- Safety check
-if [ -z "$GIT_DIR" ]; then
+if [ -z "${GIT_DIR}" ]; then
 	echo "Don't run this script from the command line." >&2
 	echo " (if you want, you could supply GIT_DIR then run" >&2
 	echo "  $0 <oldrev>..<newrev>)" >&2
 	exit 1
 fi
 
+HOOKS_DIR=${HOOKS_DIR:-${GIT_DIR}/hooks}
 tmp_msg_file=$(mktemp)
 tmp_diff_file=$(mktemp)
 
@@ -26,7 +27,7 @@ hashes=$(git rev-list "$1")
 for h in ${hashes}
 do
 	git diff ${h}^..${h} > ${tmp_diff_file}
-	${GIT_DIR}/hooks/check-diff.py ${tmp_diff_file}
+	${HOOKS_DIR}/check-diff.py ${tmp_diff_file}
 	git cat-file commit ${h} | sed '1,/^$/d' > ${tmp_msg_file}
-	${GIT_DIR}/hooks/commit-msg ${tmp_msg_file}
+	${HOOKS_DIR}/commit-msg ${tmp_msg_file}
 done
