dc5255b12e6b6aecb9900588a4f3a454b55a70b1
glx <glx@openttd.org>
2020-01-22 21:41:09 +0100

    Change: Always do all checks for all commits

--- hooks/check-commits.sh
+++ hooks/check-commits.sh
@@ -18,16 +18,13 @@ HOOKS_DIR=${HOOKS_DIR:-${GIT_DIR}/hooks}
 tmp_msg_file=$(mktemp)
 tmp_diff_file=$(mktemp)
 
-finish() {
-	rm -f ${tmp_msg_file} ${tmp_diff_file}
-}
-trap finish EXIT
-
 hashes=$(git rev-list "$1")
 for h in ${hashes}
 do
 	LC_ALL=C git diff ${h}^..${h} > ${tmp_diff_file}
-	${HOOKS_DIR}/check-diff.py ${tmp_diff_file}
+	${HOOKS_DIR}/check-diff.py ${tmp_diff_file} || true
 	git cat-file commit ${h} | sed '1,/^$/d' > ${tmp_msg_file}
-	${HOOKS_DIR}/check-message.py ${tmp_msg_file} server
+	${HOOKS_DIR}/check-message.py ${tmp_msg_file} server || true
 done
+
+rm -f ${tmp_msg_file} ${tmp_diff_file}
